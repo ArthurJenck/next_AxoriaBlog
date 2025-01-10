04ae703dcab1ae7032005da1471badd139cbe5c8
Arthur Jenck <arthurjenckdev@gmail.com>
2025-01-10 21:47:50 +0100

    feat: added correct server action

--- src/lib/serverActions/blog/postServerActions.js
+++ src/lib/serverActions/blog/postServerActions.js
@@ -2,7 +2,8 @@ import { Post } from "@/lib/models/post"
 import { connectToDb } from "@/lib/utils/db/connectToDb"
 
 export const addPost = async (formData) => {
-  const { title, markdownArticle } = Object.fromEntries(formData)
+  const [title, markdownArticle] = Object.fromEntries(formData)
+
   try {
     await connectToDb()
 
@@ -12,10 +13,12 @@ export const addPost = async (formData) => {
     })
 
     const savedPost = await newPost.save()
-    console.log("Post saved")
+    console.log("Successfully saved post")
+
     return { success: true, slug: savedPost.slug }
   } catch (error) {
-    console.error("Error while creating the post:", error)
+    console.error("Error while creating post", error)
+
     throw new Error(error.message || "An error occured while creating the post")
   }
 }
