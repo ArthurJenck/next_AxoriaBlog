b9aa7eb0ab6e85e585980ddc090eb78fb33ab11d
Arthur Jenck <arthurjenckdev@gmail.com>
2025-02-11 11:48:00 +0100

    feat: added auth pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,25 @@ import Link from "next/link"
 import React from "react"
 
 export const Navbar = () => {
-    return (
-        <nav className="fixed w-full  bg-slate-50 border-b border-b-zinc-300">
-            <div className="u-main-container flex py-4">
-                <Link href="/" className="mr-2 text-zinc-900">
-                    AXORIA
-                </Link>
-                <Link href="/categories" className="ml-2 text-zinc-900 mr-auto">
-                    Categories
-                </Link>
-                <Link href="/dashboard/create" className="mx-2 text-zinc-900">
-                    Add an article
-                </Link>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="fixed w-full  bg-slate-50 border-b border-b-zinc-300">
+      <div className="u-main-container flex py-4">
+        <Link href="/" className="mr-2 text-zinc-900">
+          AXORIA
+        </Link>
+        <Link href="/categories" className="ml-2 text-zinc-900 mr-auto">
+          Categories
+        </Link>
+        <Link href="/dashboard/create" className="mx-2 text-zinc-900">
+          Add an article
+        </Link>
+        <Link href="/signin" className="mx-2 text-zinc-900">
+          Sign in
+        </Link>
+        <Link href="/signup" className="mx-2 text-zinc-900">
+          Sign up
+        </Link>
+      </div>
+    </nav>
+  )
 }
